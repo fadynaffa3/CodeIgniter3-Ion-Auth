eb369df40b5d3b00cf076a2aa681f42a9138b5fb
Fady Naffa <fadynaffa3@gmail.com>
2015-11-14 22:50:21 +0200

    adding bootstrao form-control class

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,28 +1,28 @@
-<h1><?php echo lang('login_heading');?></h1>
-<p><?php echo lang('login_subheading');?></p>
+<div class='row'>
+  <div id="infoMessage"><?php echo $message;?></div>
+  <div style="width:50%; margin: 0 auto;">
+    <?php echo form_open("auth/login");?>
 
-<div id="infoMessage"><?php echo $message;?></div>
+    <div class="form-group">
+      <label for="identity"><?php echo t('identity');?></label>
+      <?php echo form_input($identity);?>
+    </div>
 
-<?php echo form_open("auth/login");?>
+    <div class="form-group">
+      <label for="password"><?php echo t('password');?></label>
+      <?php echo form_input($password);?>
+    </div>
 
-  <p>
-    <?php echo lang('login_identity_label', 'identity');?>
-    <?php echo form_input($identity);?>
-  </p>
-
-  <p>
-    <?php echo lang('login_password_label', 'password');?>
-    <?php echo form_input($password);?>
-  </p>
-
-  <p>
-    <?php echo lang('login_remember_label', 'remember');?>
-    <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-  </p>
+    <div class="form-group">
+      <?php echo t('remember');?>
+      <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
+    </div>
 
 
-  <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
+    <div class="form-group"><?php echo form_submit('submit', t('submit'), 'class="btn btn-success"');?></div>
 
-<?php echo form_close();?>
+    <?php echo form_close();?>
 
-<p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+    <div><a href="forgot_password"><?php echo t('forgot_password');?></a></div>
+  </div>
+</div>
